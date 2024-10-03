5bf863d947c9b63ead8cd15fbeccdca0a0c355f1
Dinis Esteves <dinis.esteves@tecnico.ulisboa.pt>
2024-10-03 16:23:11 +0100

    commit da Class HotelEntity| falta fazer os metodos the hash e equals

--- hva-skeleton/hva/core/HotelEntity.java
+++ hva-skeleton/hva/core/HotelEntity.java
@@ -1,8 +1,31 @@
 package hva.core;
 
-public class HotelEntity {
+abstract class HotelEntity {
 
     private String _id;
     private String _name;
     
+    protected HotelEntity(String id, String name)  {
+        _id = id;
+        _name = name;
+    }
+
+    protected String getId() {
+        return _id;
+    }
+
+    protected String getName(){
+        return _name;
+    }
+    
+    @Override
+    public int hashCode() {
+        // Falta Fazer o Hash dos ids
+        return 0;
+    }
+
+    public boolean equals() {
+        // Falta Fazer O Metodo de comparaçao
+        return false;
+    }
 }
